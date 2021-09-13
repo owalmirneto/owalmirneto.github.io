b85d246de2421bd3faa67a85f442a68a90f9ed91
Walmir Neto <owalmirneto@gmail.com>
2021-09-12 22:11:20 -0300

    Remove console.logs

--- javascripts/education_controller.js
+++ javascripts/education_controller.js
@@ -1,4 +1,3 @@
 $.get('/api/courses.json', function (response) {
-  console.log(response);
   new Vue({ el: '#education', data: { education: response } });
 });
--- javascripts/experience_controller.js
+++ javascripts/experience_controller.js
@@ -1,4 +1,3 @@
 $.get('/api/jobs.json', function (response) {
-  console.log(response);
   new Vue({ el: '#experience', data: { experience: response } });
 });
