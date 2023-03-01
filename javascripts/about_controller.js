87d6b50245cd13d333c9dc7d78beadd720ac7d0a
Walmir <owalmirneto@gmail.com>
2023-03-01 01:41:37 -0300

    Add link to download resume.pdf

--- javascripts/about_controller.js
+++ javascripts/about_controller.js
@@ -1,5 +1,5 @@
 $.get('/api/about.json', function (response) {
-  new Vue({ el: '#about', data: { about: response } });
+  new Vue({ el: '#about', data: { about: response, contacts: response.contacts } });
 }).done(() => {
   $('.social-icons .social-icon').tooltip()
 });
